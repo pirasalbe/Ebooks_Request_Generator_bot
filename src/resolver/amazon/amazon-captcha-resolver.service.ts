069686b18f46b7a2f7f62a0eb04bcc5128aa7d62
pirasalbe <alberto98.piras@gmail.com>
2021-11-02 23:03:49 +0100

    Detect captcha and show better message

--- src/resolver/amazon/amazon-captcha-resolver.service.ts
+++ src/resolver/amazon/amazon-captcha-resolver.service.ts
@@ -1,9 +1,18 @@
 import { HTMLElement } from 'node-html-parser';
 
-export class AmazonCaptchaResolverService {
-  private static readonly KINDLE_UNLIMITED_ID = '.a-icon-kindle-unlimited';
+import { NullableHtmlElement } from './../html/nullable-html-element';
 
-  checkCaptcha(html: HTMLElement): boolean {
-    return false;
+export class AmazonCaptchaResolverService {
+  private static readonly CAPTCHA_FORM_ID =
+    'form[action="/errors/validateCaptcha"]';
+
+  checkCaptcha(html: HTMLElement): void {
+    const captchaForm: NullableHtmlElement = html.querySelector(
+      AmazonCaptchaResolverService.CAPTCHA_FORM_ID
+    );
+
+    if (captchaForm != null) {
+      throw 'Amazon requested a captcha. Try again later or change locale (for example, amazon.co.uk instead of amazon.com).';
+    }
   }
 }
