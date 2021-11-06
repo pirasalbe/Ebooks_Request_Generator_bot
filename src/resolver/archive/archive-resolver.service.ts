dd098a25b45cc2c140d7a9fb8c23ed2513edbf4c
pirasalbe <alberto98.piras@gmail.com>
2021-11-06 17:34:20 +0100

    Support archive.org

--- src/resolver/archive/archive-resolver.service.ts
+++ src/resolver/archive/archive-resolver.service.ts
@@ -1,7 +1,10 @@
 import { HTMLElement } from 'node-html-parser';
 import { URL } from 'url';
 
+import { I18nUtil } from '../../i18n/i18n-util';
+import { LanguageStrings } from '../../i18n/language-strings';
 import { AbstractResolver } from '../abstract-resolver';
+import { Entry } from '../html/entry';
 import { Message } from '../message';
 import { SiteResolver } from '../site-resolver.enum';
 import { HtmlUtil } from './../html/html-util';
@@ -9,6 +12,8 @@ import { NullableHtmlElement } from './../html/nullable-html-element';
 import { ArchiveInformation } from './archive-information';
 
 export class ArchiveResolverService extends AbstractResolver {
+  private static readonly SITE_LANGUAGE = 'en';
+
   private static readonly CONTAINERS_ID = '.thats-left.item-details-metadata';
 
   private static readonly TITLE_ID = '.item-title';
@@ -94,18 +99,73 @@ export class ArchiveResolverService extends AbstractResolver {
     return information;
   }
 
-  private setDetails(message: Message, body: HTMLElement) {
-    const publisher: NullableHtmlElement = body.querySelector(
+  private setDetails(message: Message, body: HTMLElement): void {
+    let publisher = false;
+    let language = false;
+
+    const publisherElement: NullableHtmlElement = body.querySelector(
       ArchiveResolverService.PUBLISHER_ID
     );
 
-    if (publisher != null) {
-      message.setPublisher(HtmlUtil.getTextContent(publisher));
+    if (publisherElement != null) {
+      publisher = true;
+      message.setPublisher(HtmlUtil.getTextContent(publisherElement));
     }
+
+    const metadata: HTMLElement[] = body.querySelectorAll(
+      ArchiveResolverService.DETAILS_ID
+    );
 
     // TODO cicle metadata
     // look for:
     // * publisher if it is null
     // * language
+    for (let i = 0; i < metadata.length && (!language || !publisher); i++) {
+      const element = metadata[i];
+      const entry: Entry<string, string> = this.getDetailElement(element);
+      console.log(entry);
+      const key: string | null = I18nUtil.getKey(
+        ArchiveResolverService.SITE_LANGUAGE,
+        entry.getKey()
+      );
+
+      if (key != null) {
+        switch (key) {
+          case LanguageStrings.LANGUAGE_KEY:
+            language = true;
+            this.addLanguageTag(message, entry.getValue());
+            break;
+          case LanguageStrings.PUBLISHER_KEY:
+            if (!publisher) {
+              publisher = true;
+              message.setPublisher(entry.getValue());
+            }
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+
+  private getDetailElement(element: HTMLElement): Entry<string, string> {
+    const keyElement: NullableHtmlElement = element.querySelector('dt');
+    const valueElement: NullableHtmlElement = element.querySelector('dd');
+
+    if (keyElement != null && valueElement != null) {
+      return new Entry<string, string>(
+        HtmlUtil.getTextContent(keyElement),
+        HtmlUtil.getTextContent(valueElement)
+      );
+    } else {
+      throw 'Error parsing page. Cannot read a product detail.';
+    }
+  }
+
+  private addLanguageTag(message: Message, language: string): void {
+    const languageLowerCase: string = language.toLowerCase();
+    if (this.isLanguageTagRequired(languageLowerCase)) {
+      message.addTag(languageLowerCase);
+    }
   }
 }
