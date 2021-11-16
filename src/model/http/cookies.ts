745764debff5306fa08473eb54698dd2f1acdd28
pirasalbe <alberto98.piras@gmail.com>
2021-11-16 22:16:34 +0100

    Define startup cookies

--- src/model/http/cookies.ts
+++ src/model/http/cookies.ts
@@ -18,7 +18,8 @@ export class Cookies {
 
   private resetCookies() {
     this.cookies = new Map<string, string>();
-    this.cookiesHeader = '';
+    this.cookiesHeader =
+      'session-id=134-4712298-1130624; sp-cdn="L5Z9:IT"; ubid-main=135-8368052-7206346; s_fid=1CDBD5D9344DE5E6-0E5CF874166BD1E4; sst-main=Sst1|PQEPnphlOTd4KjHA5jmHt_HUCdFjA4soJx2FkOknHwZmikxdY3W8MKiKgN5ds9LQ6XUkRlxjhleOGf_gvdR6Y2GzU2JJtn7n1e3QlVFRBYEuFsTMDKj0JlErtLxoZ0XJCVXqnoHEydRdStdVApmbXdNSW37MYwtXAICAxY9Xeo8T6YBomM7aNmMN8JH8Hm0t6cBk7szFQdGOkDgXmhEV6SH3i7rXwfdt_85XIxwbgt0H9LGvxYNBRZlvCgBgL0JYNRphNsrBM0rpWplJldUtWKDUZ3Unmr5HSKDr4UV4QA0Sxbg; i18n-prefs=USD; session-id-time=2082787201l; lc-main=en_US; session-token=uvhCok4ph6EI98vVez+hyPqH8gAUzpZKUgT2X002oQRD1Nt9SWb0pzyRHKnwDPVOtRzKOECDeB3keKX9Q01HKREi8vI9Cw8sE5rgf2f7fsmFd+IZxPnTSjsM7ZOk7LfXiiPiqDAiOgmV8eqFeteN97nCKcETRmWeGSMzgHr5Af3g0UNJK8zijXSKwIwMngd0; csm-hit=tb:s-PP7CPPQ2NGCS0MP7497Z|1636914960486&t:1636914961866&adb:adblk_yes';
     this.resetDate = DateUtil.addHours(new Date(), Cookies.MAX_AGE_HOURS);
   }
 
