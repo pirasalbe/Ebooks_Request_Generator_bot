a92e69eb7ab5cb2ddd2ed3991b3d5e8bfc3b5fd2
pirasalbe <alberto98.piras@gmail.com>
2021-11-24 19:52:28 +0100

    Send real tweet

--- src/service/twitter/twitter.service.ts
+++ src/service/twitter/twitter.service.ts
@@ -4,6 +4,8 @@ import TwitterApiv2ReadWrite from 'twitter-api-v2/dist/v2/client.v2.write';
 import { Message } from './../../model/telegram/message';
 
 export class TwitterService {
+  static readonly TWEET_MAX_SIZE = 280;
+
   private twitterApi: TwitterApi;
   private twitterApiReadWrite: TwitterApiv2ReadWrite;
 
@@ -27,9 +29,38 @@ export class TwitterService {
     if (messages.length > 0) {
       const message: Message = messages[0];
 
-      this.twitterApiReadWrite.tweet('Test tweet').catch((err) => {
-        console.debug('Error sending tweet', err);
-      });
+      const text: string = this.toTweet(message);
+
+      if (text.length < TwitterService.TWEET_MAX_SIZE) {
+        this.twitterApiReadWrite.tweet(text).catch((err) => {
+          console.debug('Error sending tweet', err);
+        });
+      }
     }
+  }
+
+  private toTweet(message: Message): string {
+    let tweet = '';
+
+    // tags
+    const site: string = message.getSiteName().toLowerCase();
+    tweet += message.toTileString();
+    if (!tweet.includes(site)) {
+      tweet = '#' + site + ' ' + tweet;
+    }
+
+    tweet += '\n';
+
+    // info
+    const info: string = message.getTitle() + 'by ' + message.getAuthor();
+    const link: string = message.getLink();
+
+    if (tweet.length + link.length < TwitterService.TWEET_MAX_SIZE) {
+      tweet += info;
+    }
+
+    tweet += link;
+
+    return tweet;
   }
 }
